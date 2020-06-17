d3f76571ab460d5fcc81d7877a17ba3a84260995
Junior <junior.jaava@gmail.com>
2020-06-17 21:23:25 +0200

    starting with angular

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //view or template
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'alurapica';      //component in typescript
+  description = 'Leão';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg';
 }
