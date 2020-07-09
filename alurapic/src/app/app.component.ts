c989d86a80b413a500c1c7ba3307f6925fe91d30
Junior <junior.jaava@gmail.com>
2020-07-09 20:06:06 +0200

    Life cycle - ngOnInit

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
 import { Photo } from './photos/photo/photo';
 
@@ -7,17 +7,18 @@ import { Photo } from './photos/photo/photo';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  photos: Photo[] = [];
+  photos: any[] = [];
 
-  constructor(photoService: PhotoService) {
+  //constructor only to injection dependency
+  constructor(private photoService: PhotoService) { }
 
-    photoService
+
+  //it will be call after class be builded
+  ngOnInit(): void {
+    this.photoService
       .listFromUser('flavio')
-      .subscribe(photos => {
-        console.log(photos[0].description);
-        this.photos = photos
-      });
+      .subscribe(photos => this.photos = photos);
   }
 }
--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { PhotoComponent } from './photo/photo.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-    declarations: [ PhotoComponent ], // visible to all components in this module
-    exports: [ PhotoComponent ] // export or make it accessable to who call
+    declarations: [PhotoComponent], // visible to all components in this module
+    exports: [PhotoComponent], // export or make it accessable to who call
+    imports: [HttpClientModule]
 })
-export class PhotosModule {}
+export class PhotosModule { }
