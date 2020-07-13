76497c6d731be81f7dda5865c101091b48eb459d
Junior <junior.jaava@gmail.com>
2020-07-13 20:07:01 +0200

    module was auto generated

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { PhotoComponent } from './photo/photo.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
-    declarations: [PhotoComponent], // visible to all components in this module
+    declarations: [PhotoComponent, PhotoListComponent], // visible to all components in this module
     exports: [PhotoComponent], // export or make it accessable to who call
     imports: [HttpClientModule]
 })
