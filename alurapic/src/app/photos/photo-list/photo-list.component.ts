bafffd71cb494c14ffc7bcfbab8933ac63592da0
Junior <junior.jaava@gmail.com>
2020-07-28 14:55:30 +0200

    Pattern Debounce/Observer - Subject from RxJS - wait 300ms

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -7,15 +10,24 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))                    // this subscribe will be trigger after 300ms no keyuping
+      .subscribe(filter => this.filter = filter); // the subscribe going to be all the time listening for filter
+                                                  // this observable is never done, it will be always running
+  }
+
+  ngOnDestroy(): void { // when I leave this page the debounce will be destroyed.
+    this.debounce.unsubscribe();
   }
 
 }
