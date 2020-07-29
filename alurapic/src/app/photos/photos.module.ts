c61359e4c18ad4255b011adb8ec88f67eab7491b
Junior <junior.jaava@gmail.com>
2020-07-29 13:18:32 +0200

    Submodels - Is used to let the application better organize. You know exactly which component each model are using.

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+
 
 @NgModule({
-    declarations: [
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
-    ], // visible to all components in this module
     imports: [
-        HttpClientModule,
-        CommonModule
+        PhotoModule,        //sub-modules are imported. And components are in declarations.
+        PhotoFormModule,
+        PhotoListModule
     ]
 })
 export class PhotosModule { }
