52057e200489503a09924a0fb02a98bbda53c3ad
Junior <junior.jaava@gmail.com>
2020-08-01 19:33:07 +0200

    Model Driver Forms = Validation Connection between component class form and template html form Safe Navigation Operation - if errors is an array/exists, evaluate required

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,6 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SignInComponent { }
+export class SignInComponent implements OnInit {
+
+    loginForm: FormGroup;   //this property going to controle/validate the form in template file
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({ //responsible in create the form and attributes in it
+            userName: ['', Validators.required], //default values, validations
+            password: ['', Validators.required]  //check the template
+        });
+    }
+}
