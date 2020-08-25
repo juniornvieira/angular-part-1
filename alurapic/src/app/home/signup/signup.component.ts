01b8bccd5acf6eec3d849019adc88efc4bc73a72
Junior <junior.jaava@gmail.com>
2020-08-25 20:00:21 +0200

    signup process - submit form - post method and redirect to root

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
@@ -12,7 +15,9 @@ export class SignUpComponent implements OnInit {
 
     constructor(
         private formBuilder: FormBuilder,
-        private userNotTakenValidatorService: UserNotTakenValidatorService) { }
+        private userNotTakenValidatorService: UserNotTakenValidatorService,
+        private signUpService: SignUpService,
+        private router: Router) { }
 
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
@@ -48,5 +53,15 @@ export class SignUpComponent implements OnInit {
                 ]
             ]
         });
+    }
+
+    signup() {
+        const newUser = this.signupForm.getRawValue() as NewUser; // getRawValue return all form values. Now I've the typing check
+        this.signUpService
+            .signup(newUser)
+            .subscribe( //if everything is ok, send to root ''
+                () => this.router.navigate(['']),
+                err => console.log(err)
+            );
     }
 }
