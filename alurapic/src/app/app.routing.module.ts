94ab999faa5c818a6ee5b0692d11924b5fa2de8b
Junior <junior.jaava@gmail.com>
2020-08-27 20:24:10 +0200

    Implementing lazy load. First page > father(main.js) or first page to be load is user/flavio Now, when click on login(/home/), home-home-module.js (or child) will be load. After these two loads (main, home-module), the modules won’t be loaded anymore. Single page application = load just once. Now, I don’t have a big module anymore. After production build, I do have two small module

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -5,29 +5,16 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
-import { SignInComponent } from './home/signin/signin.component';
-import { AuthGuard } from './core/auth/auth.guard';
-import { SignUpComponent } from './home/signup/signup.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
     {
-        path: '',//father homecomponent has two children
-        component: HomeComponent,
-        canActivate: [AuthGuard],
-        children: [
-            {
-                path: '', //first child
-                component: SignInComponent,
-            },
-            {
-                path: 'signup', //second child
-                component: SignUpComponent,
-            },
-        ]
-        // Com isso, teremos um caminho para SignInComponent e para SignUpComponent, e se acessarmos localhost:4200, teremos HomeComponent. 
-        // Por conta do seu path, que é igual à da rota filha, o SignInComponent será exibido primeiro. No entanto, se clicarmos no link de SignInComponent 
-        // para acessarmos a página de cadastro de usuário, é o SignUpComponent que será carregado, dentro do Router outlet do HomeComponent.
+        path: '',
+        pathMatch: 'full', // path should be execlty like ''
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        loadChildren: './home/home.module#HomeModule'
     },
     {
         path: 'user/:userName',         // when the path is called
